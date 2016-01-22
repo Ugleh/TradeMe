d2dcc073c7b6036723ec1925d53826473245c42e
JosephRobidoux <troti@ymail.com>
2016-01-22 11:21:29 -0500

    Added more comments

--- src/com/ugleh/trademe/TradeMeListener.java
+++ src/com/ugleh/trademe/TradeMeListener.java
@@ -16,27 +16,27 @@ import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
 
 public class TradeMeListener implements Listener {
-	String chatPrefix = ChatColor.DARK_PURPLE + "[" + ChatColor.LIGHT_PURPLE + "TRADE" + ChatColor.DARK_PURPLE + "] "
-			+ ChatColor.RESET;
-
 	@EventHandler
 	public void PlayerQuitEvent(PlayerQuitEvent e) {
+		//If either player quits, we cancel the trade.
 		if (CommandTrade.tradeList.containsKey(e.getPlayer()) || CommandTrade.tradeList.containsValue(e.getPlayer())) {
 			CommandTrade.tradeListKey(e.getPlayer())
-					.sendMessage(chatPrefix + ChatColor.RED + "The person you attempted to trade with has left.");
+					.sendMessage(TradeUtils.chatPrefix + ChatColor.RED + "The person you attempted to trade with has left.");
 			CommandTrade.tradeCancel(e.getPlayer());
 		}
 	}
 
 	@EventHandler
 	public void InventoryClickEvent(InventoryClickEvent e) {
+		//Your Offer lets you modify anything except for the bottom row of the top inventory.
 		if (e.getInventory().getName().toLowerCase().contains("your offer")) {
 			if (e.getRawSlot() == 30) {
 				e.setCancelled(true);
 				CommandTrade.tradeDone((Player) e.getWhoClicked());
-				// Accept Trade
+				// Accept Trade Offer
 			} else if (e.getRawSlot() == 32) {
 				e.setCancelled(true);
+				//Decline Trade Offer
 				CommandTrade.tradeCancel((Player) e.getWhoClicked());
 			} else if (e.getRawSlot() >= 36) {
 				// Nothing
@@ -45,15 +45,16 @@ public class TradeMeListener implements Listener {
 			}
 		}
 		if (e.getInventory().getName().toLowerCase().contains("their offer")) {
+			//Their Offer Inventory should not be modified at all.
 			e.setCancelled(true);
 			if (e.getRawSlot() == 30) {
 				e.setCancelled(true);
-				CommandTrade.approvedTrade((Player) e.getWhoClicked());
 				// Accept Trade
+				CommandTrade.approvedTrade((Player) e.getWhoClicked());
 			} else if (e.getRawSlot() == 32) {
 				e.setCancelled(true);
-				CommandTrade.tradeCancel((Player) e.getWhoClicked());
 				// Decline Trade
+				CommandTrade.tradeCancel((Player) e.getWhoClicked());
 			}
 		}
 
@@ -64,13 +65,9 @@ public class TradeMeListener implements Listener {
 		Player p = (Player) e.getPlayer();
 		if(CommandTrade.finalizedTrades.containsKey(p)) return;
 		if (CommandTrade.tradeList.containsKey(p) || CommandTrade.tradeList.containsValue(p)) {
-			if (e.getInventory().equals(CommandTrade.currentlyTrading.get(p))) // Left
-																				// the
-																				// active
-																				// trade
-																				// inventory.
-			{
-				p.sendMessage(chatPrefix + ChatColor.GOLD + " You left an active trade. What will you do?");
+			if (e.getInventory().equals(CommandTrade.currentlyTrading.get(p)))
+			{//User left the active trade window.
+				p.sendMessage(TradeUtils.chatPrefix + ChatColor.GOLD + " You left an active trade. What will you do?");
 				PacketPlayOutChat packet = new PacketPlayOutChat(ChatSerializer.a(
 						"[\"\",{\"text\":\">>>>\",\"color\":\"dark_blue\",\"bold\":true},{\"text\":\"[ \",\"color\":\"gold\",\"bold\":true},{\"text\":\"RETURN\",\"color\":\"green\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/trade return\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Click to return to trade.\",\"color\":\"green\"}]}}},{\"text\":\" | \",\"color\":\"black\",\"bold\":true},{\"text\":\"CANCEL\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/trade cancel\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Click to cancel trade.\",\"color\":\"red\"}]}}},{\"text\":\" ]\",\"color\":\"gold\",\"bold\":true},{\"text\":\"<<<<\",\"color\":\"dark_blue\",\"bold\":true}]"));
 				((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
